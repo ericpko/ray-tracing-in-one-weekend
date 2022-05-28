ee401a45d4d75f12de87413b7ac07b7f783cbd1f
Eric K <eric.koehli@mail.utoronto.ca>
2022-05-28 10:44:33 -0400

    Refactored code to write to a ppm image rather than pipe output from stdout into file

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
 // Image dimensions in pixels
-const IMAGE_WIDTH: u32 = 256;
-const IMAGE_HEIGHT: u32 = 256;
+const IMAGE_WIDTH: usize = 256;
+const IMAGE_HEIGHT: usize = 256;
 
 pub fn render() -> anyhow::Result<()> {
     pretty_env_logger::init();
     log::info!("rendering image...");
 
-    println!("P3\n{IMAGE_WIDTH} {IMAGE_HEIGHT}\n255");
+    let mut image: Vec<u8> = Vec::with_capacity(3 * IMAGE_WIDTH * IMAGE_HEIGHT);
 
     for j in (0..IMAGE_HEIGHT).rev() {
         for i in 0..IMAGE_WIDTH {
@@ -18,9 +18,20 @@ pub fn render() -> anyhow::Result<()> {
             let ig = (255.999 * g) as u8;
             let ib = (255.999 * b) as u8;
 
-            println!("{ir} {ig} {ib}");
+            image.extend([ir, ig, ib]);
         }
     }
+    write_ppm(image)?;
 
+    Ok(())
+}
+
+fn write_ppm(image: Vec<u8>) -> std::io::Result<()> {
+    let mut image_vec = format!("P6\n{IMAGE_WIDTH} {IMAGE_HEIGHT}\n255\n")
+        .as_bytes()
+        .to_owned(); // &[u8] -> Vec<u8>
+
+    image_vec.extend(image);
+    std::fs::write("image.ppm", image_vec)?;
     Ok(())
 }
