16b3748e07f073711c55c12c8c915225cba0dfee
Eric K <eric.koehli@mail.utoronto.ca>
2022-05-30 16:29:34 -0400

    Completed 6.1

--- src/lib.rs
+++ src/lib.rs
@@ -25,18 +25,29 @@ pub fn render(mut image: Image, camera: Camera) -> anyhow::Result<()> {
     Ok(())
 }
 
-fn hit_sphere(center: Vec3, radius: f32, ray: &Ray) -> bool {
+fn hit_sphere(center: Vec3, radius: f32, ray: &Ray) -> f32 {
     let oc = ray.origin - center;
     let a = ray.dir.dot(ray.dir);
     let b = 2.0 * oc.dot(ray.dir);
     let c = oc.dot(oc) - radius * radius;
     let discriminant = b * b - 4.0 * a * c;
-    discriminant > 0.0
+    if discriminant < 0. {
+        return -1.;
+    } else {
+        return (-b - discriminant.sqrt()) / (2.0 * a);
+    }
 }
 
 fn ray_color(ray: Ray) -> Vec<u8> {
-    if hit_sphere(Vec3::new(0., 0., -1.), 0.5, &ray) {
-        return vec![255_u8, 0_u8, 0_u8];
+    let t = hit_sphere(Vec3::new(0., 0., -1.), 0.5, &ray);
+    if t > 0.0 {
+        let n = (ray.at(t) - Vec3::new(0., 0., -1.)).normalize();
+        let color = 0.5 * Vec3::new(n.x + 1., n.y + 1., n.z + 1.);
+        return vec![
+            (color.x * 255.999) as u8,
+            (color.y * 255.999) as u8,
+            (color.z * 255.999) as u8,
+        ];
     }
     let unit_dir = ray.dir.normalize();
     let t = 0.5 * (unit_dir.y + 1.0);
@@ -52,7 +63,7 @@ fn ray_color(ray: Ray) -> Vec<u8> {
 fn write_ppm(image: Image) -> std::io::Result<()> {
     let width = image.width;
     let height = image.height;
-    let mut image_vec = format!("P6\n{width} {height}\n255\n").as_bytes().to_owned(); // &[u8] -> Vec<u8>
+    let mut image_vec = format!("P6\n{width} {height}\n255\n").as_bytes().to_owned();
 
     image_vec.extend(image.pixels);
     std::fs::write("image.ppm", image_vec)?;
