ec9b62f2ddc0b20901a453adb9db5432d35837f7
Eric K <eric.koehli@mail.utoronto.ca>
2022-05-30 16:33:09 -0400

    Refactored hit_sphere. Completed 6.2

--- src/lib.rs
+++ src/lib.rs
@@ -27,14 +27,15 @@ pub fn render(mut image: Image, camera: Camera) -> anyhow::Result<()> {
 
 fn hit_sphere(center: Vec3, radius: f32, ray: &Ray) -> f32 {
     let oc = ray.origin - center;
-    let a = ray.dir.dot(ray.dir);
-    let b = 2.0 * oc.dot(ray.dir);
-    let c = oc.dot(oc) - radius * radius;
-    let discriminant = b * b - 4.0 * a * c;
-    if discriminant < 0. {
-        return -1.;
+    let a = ray.dir.length_squared();
+    let half_b = oc.dot(ray.dir);
+    let c = oc.length_squared() - radius * radius;
+    let discriminant = half_b * half_b - a * c;
+
+    if discriminant < 0.0 {
+        return -1.0;
     } else {
-        return (-b - discriminant.sqrt()) / (2.0 * a);
+        return (-half_b - discriminant.sqrt()) / a;
     }
 }
 
